049a3717f025b509c2867e5c05d9ec82d58e9802
fengjiawen <fengjiawen@youmi.net>
2014-08-12 15:25:34 +0800

    界面标识的中文常量已全部迁移到工具类UmipaytestUtils中

--- UmipayTest/src/com/umipay/android/umipaysdkdemo/test/SDKTest.java
+++ UmipayTest/src/com/umipay/android/umipaysdkdemo/test/SDKTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import net.umipay.android.view.UmipayActivity;
 import android.R.bool;
 import android.R.integer;
+import android.R.string;
 import android.content.res.Configuration;
 import android.graphics.AvoidXfermode.Mode;
 import android.os.Environment;
@@ -27,8 +28,6 @@ import com.robotium.solo.WebElement;
 public class SDKTest extends ActivityInstrumentationTestCase2 {
 	private Solo solo;// 声明Solo
 
-	private String name = "hello2";
-	private String pass = "123456";
 	final String TAG = "调试输出";
 
 	public SDKTest()// 构造方法
@@ -59,32 +58,29 @@ public class SDKTest extends ActivityInstrumentationTestCase2 {
 	public void test01Regisrtation() throws Exception {
 
 		boolean isOK=false;
-		solo.clickOnText("注册登录");
-
-//		String activityname = getActivity().getClass().getName();
-//		Log.d(TAG, activityname);
-		solo.clickOnText("注册帐号");
+		solo.clickOnText(UmipaytestUtils.MAINPAGE_BT_REGISTRATION_LOGIN);
+		solo.clickOnText(UmipaytestUtils.REGISTRATIONPAGE_TAB_REGISTRATION);
 		solo.clearEditText(0);
-		solo.enterText(0, name);
+		solo.typeText(0, UmipaytestUtils.REGISTRATIONPAGE_ET_NAME);
 		solo.clearEditText(1);
-		solo.enterText(1, pass);
-		solo.clickOnText("偶玩服务条款");
-		assertTrue(solo.waitForText("偶玩游戏平台用户协议"));
+		solo.typeText(1, UmipaytestUtils.REGISTRATIONPAGE_ET_PASSWORD);
+		solo.clickOnText(UmipaytestUtils.REGISTRATIONPAGE_LINK_OUWANSERVER);
+		assertTrue(solo.waitForText(UmipaytestUtils.OUWANSERVERPAGE_CONTENT));
 		solo.goBack(); //solo.clickOnImage(0);
-		//solo.clickOnCheckBox(0);
-		solo.clickOnButton("立即注册");
+		solo.clickOnCheckBox(0);
+		solo.clickOnButton(UmipaytestUtils.REGISTRATIONPAGE_BT_REGISTRATION);
 		// assertEquals(false, solo.searchText("请输入6～32位长度的密码"));
 		/*
 		 * solo.sleep(120000); activityname=getActivity().getClass().getName();
 		 * Log.d(TAG,activityname );
 		 */
-		solo.waitForText("网络正在通讯中，请稍等");
+		solo.waitForText(UmipaytestUtils.WAIT_NATWORK_COMUNICATION);
 		if(isMainPage()){
-			solo.clickOnText("账户中心");
+			solo.clickOnText(UmipaytestUtils.MAINPAGE_BT_ACCOUNT);
 			isOK=isAccountPage();
 			solo.takeScreenshot("Registratuion_01");
 		}
-		Log.i(TAG, "Registratuion"+isOK);
+		Log.i(TAG, "Registratuion_结果"+isOK);
 	}
 
 	/*
@@ -95,28 +91,23 @@ public class SDKTest extends ActivityInstrumentationTestCase2 {
 
 		boolean isOK = false;
 
-		solo.clickOnText("注册登录");
-		solo.sleep(2000);
-		String activityname = getActivity().getClass().getName();
-		Log.d(TAG, "login_1  " + activityname);
+		solo.clickOnText(UmipaytestUtils.MAINPAGE_BT_REGISTRATION_LOGIN);
 		solo.takeScreenshot("Login_1_主界面");
 		solo.clearEditText(0);
 		solo.enterText(0, "hello01234");
 		solo.clearEditText(1);
 		solo.enterText(1, "123456");
-		assertEquals("选中记住密码的复选框！", true, !solo.isCheckBoxChecked(0));
-		solo.clickOnButton("登录");
-		// activityname = getActivity().getClass().getName();
-		// Log.d(TAG, "login_2  " + activityname);
+		assertEquals("选中记住密码的复选框！", true, !solo.isCheckBoxChecked(0)); 
+		solo.clickOnButton(UmipaytestUtils.LOGINPAGE_BT_LOGIN);
 
-		long time = waitTime("网络正在通讯中，请稍等");
+		long time = waitTime(UmipaytestUtils.WAIT_NATWORK_COMUNICATION);
 		Log.d(TAG, "Login_登录所用时间_" + time);
 		if (isBindphonePage()) {
-			solo.clickOnText("跳过此步");
+			solo.clickOnText(UmipaytestUtils.BINDPHONEPAGE_BT_IGNORE);
 		}
-		solo.waitForText("账户中心");
-		solo.clickOnText("账户中心");
-		time = waitTime("正在努力加载中，请稍等");
+		solo.waitForText(UmipaytestUtils.MAINPAGE_BT_ACCOUNT);
+		solo.clickOnText(UmipaytestUtils.MAINPAGE_BT_ACCOUNT);
+		time = waitTime(UmipaytestUtils.WAIT_LOADING);
 		// 通过点击账户中心无须登录验证已经成功登录
 		if (isAccountPage()) {
 			Log.d(TAG, "Login_进入账户中心所用时间_" + time);
@@ -138,38 +129,34 @@ public class SDKTest extends ActivityInstrumentationTestCase2 {
 	@Test
 	public void test03RecordInAccount() {
 		boolean isOK = false;
-		solo.clickOnText("账户中心");
-		solo.clickOnButton("登录");
+		solo.clickOnText(UmipaytestUtils.MAINPAGE_BT_ACCOUNT);
+		solo.clickOnButton(UmipaytestUtils.LOGINPAGE_BT_LOGIN);
 
 		if (isBindphonePage()) {
-			solo.clickOnText("跳过此步");
+			solo.clickOnText(UmipaytestUtils.BINDPHONEPAGE_BT_IGNORE);
 		}
-
-		long time = waitTime("网络正在通讯中，请稍等");
+		long time = waitTime(UmipaytestUtils.WAIT_NATWORK_COMUNICATION);
 		Log.d(TAG, "RecordInAccount_登录所用时间_" + time);
 		if (isBindphonePage()) {
-			solo.clickOnText("跳过此步");
+			solo.clickOnText(UmipaytestUtils.BINDPHONEPAGE_BT_IGNORE);
 		}
-
-		solo.clickOnText("账户中心");
-		time = waitTime("正在努力加载中，请稍等");
-
+		solo.clickOnText(UmipaytestUtils.MAINPAGE_BT_ACCOUNT);
+		time = waitTime(UmipaytestUtils.WAIT_LOADING);
 		if (isAccountPage()) {
 			Log.d(TAG, "RecordInAccount_进入账户中心所用时间_" + time);
-			solo.clickOnText("订单记录");
-			solo.waitForWebElement(By.textContent("返回账户中心"));
+			solo.clickOnText(UmipaytestUtils.ACCOUNTPAGE_WEB_RECORD);
+			solo.waitForWebElement(By.textContent(UmipaytestUtils.RECORDPAGE_WEB__RETURN));
 			// time = waitTime("返回账户中心");
-			if (solo.waitForText("请求错误，请检查网络设置")
-					|| solo.waitForText("获取用户订单失败")) {
-				/***************** 用searchfor？ *************/
+			if (solo.waitForText(UmipaytestUtils.REQUEST_ERROR)
+					|| solo.waitForText(UmipaytestUtils.GET_RECORD_FIALED)) {
 
 				Log.i(TAG, "获取用户订单失败   或者  请求错误，请检查网络设置");
 
 			} else {
 				Log.d(TAG, "RecordInAccount_进入订单记录所用时间" + time);
-				getWebs("订单记录");
+
 				solo.takeScreenshot("RecordInAccount_1_订单记录");
-				solo.clickOnText("返回账户中心");
+				solo.clickOnText(UmipaytestUtils.RECORDPAGE_WEB__RETURN);
 				assertTrue("点击返回账户中心按钮无法返回账户中心", isAccountPage());
 				isOK = true;
 			}
@@ -186,58 +173,56 @@ public class SDKTest extends ActivityInstrumentationTestCase2 {
 	public void test04FeedbackInAccount() {
 
 		boolean isOk = false;
-		// 填入的反馈内容
-		String feefbackcontent = "你好";
+		String feedback_content=UmipaytestUtils.FEEDBACK_CONTENT;
+		solo.clickOnText(UmipaytestUtils.MAINPAGE_BT_ACCOUNT);
+		solo.clickOnButton(UmipaytestUtils.LOGINPAGE_BT_LOGIN);
 
-		solo.clickOnText("账户中心");
-		solo.clickOnButton("登录");
-
-		long time = waitTime("网络正在通讯中，请稍等");
+		long time = waitTime(UmipaytestUtils.WAIT_NATWORK_COMUNICATION);
 		Log.d(TAG, "FeedbackInAccount_登录所用时间" + time);
 		if (isBindphonePage()) {
-			solo.clickOnText("跳过此步");
+			solo.clickOnText(UmipaytestUtils.BINDPHONEPAGE_BT_IGNORE);
 		}
 
-		solo.clickOnText("账户中心");
-		time = waitTime("正在努力加载中，请稍等");
+		solo.clickOnText(UmipaytestUtils.MAINPAGE_BT_ACCOUNT);
+		time = waitTime(UmipaytestUtils.WAIT_LOADING);
 		if (isAccountPage()) {
 			Log.d(TAG, "FeedbackInAccount_1_进入账户中心所用时间" + time);
-			solo.clickOnText("问题反馈");
-			solo.waitForWebElement(By.textContent("添加反馈"));
+			solo.clickOnText(UmipaytestUtils.ACCOUNTPAGE_WEB_FEEDBACK);
+			solo.waitForWebElement(By.textContent(UmipaytestUtils.FEEDBACKPAGE_WEB_INSERT));
 			solo.takeScreenshot("FeedbackInAccount_2_意见反馈列表"); // 有已经发表过的反馈意见
-			getWebs("意见反馈列表");
-			solo.clickOnText("添加反馈");
-			waitTime("正在努力加载中，请稍等");// solo.waitForWebElement(By.textContent("提 交"));
+
+			solo.clickOnText(UmipaytestUtils.FEEDBACKPAGE_WEB_INSERT);
+			waitTime(UmipaytestUtils.WAIT_LOADING);// solo.waitForWebElement(By.textContent("提 交"));
 									// /****is right*****/
 			Log.d(TAG, "进入添加意见反馈界面所需时间" + time);
-			getWebs("添加意见反馈");
+
 			solo.takeScreenshot("FeedbackInAccount_3_添加意见反馈");
 			assertTrue("点击添加意见反馈按钮进入意见反馈页面", isFeedbackPage());
-			solo.clickOnText("提交");
+			solo.clickOnText(UmipaytestUtils.FEEDBACKPAGE_WEB_SUBMIT);
 
 			// 反馈内容输入框空输入，提示“请填写反馈内容”
-			if (solo.waitForText("请填写反馈内容")) {
+			if (solo.waitForText(UmipaytestUtils.FEEDBACK_CONTENT_INPUT_ERROR)) {
 				// WebElement
 				// webElement=solo.getWebElement(By.textContent("请填写反馈内容"),0);
 				// webElement.setTextContent("你好");
-				solo.enterTextInWebElement(By.textContent("请填写反馈内容"),
-						feefbackcontent);
-				solo.sleep(2000);
+//				solo.enterTextInWebElement(By.textContent("请填写反馈内容"),feedback_content);
+//				solo.sleep(2000);
+				solo.typeTextInWebElement(By.id("content"), feedback_content);
 				solo.takeScreenshot("FeedbackInAccount_4_填写反馈内容");
 			}
-			solo.clickOnText("提 交");
+			solo.clickOnText(UmipaytestUtils.FEEDBACKPAGE_WEB_SUBMIT);
 			// 不选择反馈类型，提示“请选择反馈类型”
-			if (solo.waitForText("请选择反馈类型")) {
+			if (solo.waitForText(UmipaytestUtils.FEEDBACK_TYPE_ERROR)) {
 				// WebElement
 				// webElement=solo.getWebElement(By.textContent("充值问题"), 0);
-				solo.clickOnText("充值问题");
+				solo.clickOnText(UmipaytestUtils.FEEDBACK_TYPE_CHARGE);
 				solo.sleep(2000);
 				solo.takeScreenshot("FeedbackInAccount_5_选择反馈类型");
 			}
-			solo.clickOnText("提 交");
+			solo.clickOnText(UmipaytestUtils.FEEDBACKPAGE_WEB_SUBMIT);
 			// 正确输入，提交能正确显示在意见反馈列表
 			if (isFeedbackPage()) {
-				solo.searchText(feefbackcontent);
+				solo.searchText(feedback_content);
 				isOk = true;
 				solo.takeScreenshot("FeedbackInAccount_6_添加后的意见反馈列表");
 			}
@@ -252,23 +237,23 @@ public class SDKTest extends ActivityInstrumentationTestCase2 {
 	@Test
 	public void test05BindemailInAccount() {
 		boolean isOK = false;
-		solo.clickOnText("账户中心");
-		solo.clickOnButton("登录");
+		solo.clickOnText(UmipaytestUtils.MAINPAGE_BT_ACCOUNT);
+		solo.clickOnButton(UmipaytestUtils.LOGINPAGE_BT_LOGIN);
 
-		long time = waitTime("网络正在通讯中，请稍等");
+		long time = waitTime(UmipaytestUtils.WAIT_NATWORK_COMUNICATION);;
 		Log.d(TAG, "BindemailIn_登录所用时间_" + time);
 		if (isBindphonePage()) {
-			solo.clickOnText("跳过此步");
+			solo.clickOnText(UmipaytestUtils.BINDPHONEPAGE_BT_IGNORE);
 		}
 
-		solo.clickOnText("账户中心");
-		time = waitTime("正在努力加载中，请稍等");
+		solo.clickOnText(UmipaytestUtils.MAINPAGE_BT_ACCOUNT);
+		time = waitTime(UmipaytestUtils.WAIT_LOADING);
 
 		if (isAccountPage()) {
 			Log.d(TAG, "RecordInAccount_进入账户中心所用时间_" + time);
-			solo.clickOnText("绑定邮箱");
-			solo.waitForWebElement(By.textContent("邮箱绑定"));
-			getWebs("绑定邮箱");
+			solo.clickOnText(UmipaytestUtils.ACCOUNTPAGE_WEB_BINDEMAIL);
+			solo.waitForWebElement(By.textContent(UmipaytestUtils.ACCOUNTPAGE_WEB_BINDEMAIL));
+
 			solo.takeScreenshot("BindemailInAccount_1_绑定邮箱");
 			solo.clickOnWebElement(By
 					.className("abs db ml10 fs20 c2 icon-arrow-left"));
@@ -332,27 +317,6 @@ public class SDKTest extends ActivityInstrumentationTestCase2 {
 		}
 		long endTime = SystemClock.uptimeMillis();
 		return (endTime - startTime) / 1000;
-	}
-
-	/*
-	 * 获取web的元素
-	 */
-	private void getWebs(String text) {
-		String tag = "elements";
-		ArrayList<WebElement> aList = solo.getWebElements();
-		Log.v(tag, "start " + text);
-		for (int i = 0; i < aList.size(); i++) {
-			Log.d(tag, "第" + i + "个元素  ClassName()="
-					+ aList.get(i).getClassName() + "   Id()="
-					+ aList.get(i).getId() + "   name()="
-					+ aList.get(i).getName() + "   tagName()="
-					+ aList.get(i).getTagName() + "   Text()"
-					+ aList.get(i).getText());
-
-			// Log.d(tag, "第" + i + "个元素 "+ aList.get(i).toString());
-
-		}
-		Log.v(tag, "end " + text);
 	}
 
 }
--- UmipayTest/src/com/umipay/android/umipaysdkdemo/test/UmipaytestUtils.java
+++ UmipayTest/src/com/umipay/android/umipaysdkdemo/test/UmipaytestUtils.java
@@ -14,7 +14,7 @@ public class UmipaytestUtils {
 	
 	//注册界面
 	public static final String REGISTRATIONPAGE_TAB_REGISTRATION="注册帐号";
-	public static final String REGISTRATIONPAGE_ET_NAME="hell01234";
+	public static final String REGISTRATIONPAGE_ET_NAME="hello01234hello";
 	public static final String REGISTRATIONPAGE_ET_PASSWORD="123456";
 	public static  final String REGISTRATIONPAGE_LINK_OUWANSERVER="偶玩服务条款";
 	public static final String  OUWANSERVERPAGE_CONTENT="偶玩游戏平台用户协议";
@@ -29,28 +29,30 @@ public class UmipaytestUtils {
 	//账户中心界面
 	public static final String ACCOUNTPAGE_WEB_RECORD="订单记录";
 	public static final String ACCOUNTPAGE_WEB_FEEDBACK="问题反馈";
-	public static final String  ACCOUNTPAGE_WEB_BINDEMAIL="";
+	public static final String  ACCOUNTPAGE_WEB_BINDEMAIL="绑定邮箱";
 	
 	//订单记录界面
 	public static final String RECORDPAGE_WEB__RETURN="返回账户中心";
 	
 	//意见反馈界面
 	public static final String FEEDBACKPAGE_WEB_INSERT="添加反馈";
-	public static final String FEEDBACKPAGE_WEB_SUBMIT="提交";
-	public static enum FEEDBACKPAGE_WEB_TYPE{
-		//充值问题,帐号问题,其他
-		RED,YELLO,BLUE
-	}
+	public static final String FEEDBACKPAGE_WEB_SUBMIT="提 交";
+	public static final String FEEDBACK_CONTENT="你好";
 	
+	public static final String FEEDBACK_TYPE_CHARGE="充值问题";
+	public static final String FEEDBACK_TYPE_ACCOUNT="账户问题";
+	public static final String FEEDBACK_TYPE_OTHER="其他";
+	
+
+	//充值问题,帐号问题,其他
+
 	//提示信息
 	public static final String WAIT_NATWORK_COMUNICATION="网络正在通讯中，请稍等";
 	public static final String WAIT_LOADING="正在努力加载中，请稍等";
 	public static final String REQUEST_ERROR="请求错误，请检查网络设置";
 	public static final String GET_RECORD_FIALED="获取用户订单失败";
+	public static final String FEEDBACK_CONTENT_INPUT_ERROR="请填写反馈内容";
+	public static final String FEEDBACK_TYPE_ERROR="请选择反馈类型";
 	
-	public static void main(String args[]){
-		System.out.println(UmipaytestUtils.FEEDBACKPAGE_WEB_TYPE.RED);
-		System.out.println(UmipaytestUtils.FEEDBACKPAGE_WEB_TYPE.BLUE);
-		System.out.println(UmipaytestUtils.FEEDBACKPAGE_WEB_TYPE.YELLO);
-	}
+
 }
